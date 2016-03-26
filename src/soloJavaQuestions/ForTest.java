75297679a53869ac802fc12fa985fc79b9f518ba
BliznyukS <bliznyuk.sergey@gmail.com>
2016-03-26 20:25:09 +0200

    Robot Array

---
+++ src/soloJavaQuestions/ForTest.java
@@ -0,0 +1,10 @@
+package soloJavaQuestions;
+
+public class ForTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Готово !");
+    }
+}
